43fcde71032979b90f6e643923f1db5aedaad606
Chris Brew <cbrew@acm.org>
2012-08-16 03:45:08 -0400

    submission24

--- vectorize.py
+++ vectorize.py
@@ -5,6 +5,8 @@ import numpy as np
 import os
 import itertools
 import logging
+import datetime
+
 
 logging.basicConfig(filename="vectorize.log",mode='w',format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 train = pandas.read_table('Data/train.csv',sep=',')
@@ -15,13 +17,16 @@ class MyLogisticRegression(linear_model.LogisticRegression):
         return linear_model.LogisticRegression.predict_proba(self, X)[:,1]
 
 
+clf_name = "sgd"
 
+clfs = dict(sgd=linear_model.SGDRegressor)
 
 pipeline = pipeline.Pipeline([
     ('vect', feature_extraction.text.CountVectorizer(lowercase=True,max_n=2)),
     ('tfidf', feature_extraction.text.TfidfTransformer()),
-    ('clf', linear_model.SGDRegressor()),
+    ('clf',clfs[clf_name] ()), 
 ])
+
 
 parameters = {
 # uncommenting more parameters will give better exploring power but will
@@ -31,21 +36,29 @@ parameters = {
     #  'vect__lowercase': (True,False),
 	
     # 'vect__max_features': (None, 10, 50, 100, 500,1000,5000, 10000, 50000),
-    'vect__max_n': (1,2),  # words,bigrams,trigrams
+    'vect__max_n': (1,2,3),  # words,bigrams,trigrams
 #    'tfidf__use_idf': (True, False),
 #    'tfidf__norm': ('l1', 'l2'),
     # 'clf__C': (1e-2,1,1e+2),
     # 'clf__C': (10,15,20,21,22,23,24,25,26,27,28,29,30,35,40,45,50,55,60,65,70,75,80,85,100,1000),
     # 'clf__tol': (1e-3,1e-4),
     'clf__penalty': ("l1","elasticnet"),
-    'clf__alpha': 10.0**-np.arange(6.5,8.0,step=0.5),
-    'clf__n_iter': (1000,1500,2000),
+    'clf__alpha': 10.0**-np.arange(6.5,8.0,step=0.25),
+    'clf__n_iter': (1000,1250,1500,1750,2000),
 }
 
 
+def save_predictions(ypred,i):
+    for x in itertools.count(1):
+        filename = "%s_fold_%d_%d"   % (clf_name,i,x)
+        if os.path.exists(filename):
+            next
+        else:			
+            np.save(filename,ypred)
+            logging.info("saved to %s" % filename)
+            break
 
-
-clf =  grid_search.GridSearchCV(pipeline, parameters, n_jobs=2,score_func=ml_metrics.auc,verbose=1)  
+clf =  grid_search.GridSearchCV(pipeline, parameters, n_jobs=2,score_func=ml_metrics.auc,verbose=3)  
 
 			
 ss = 0
@@ -61,6 +74,7 @@ for i,(train_i,test_i) in enumerate(kf):
 	ypred = clf.predict(train[train_i].Comment) 
 	logging.info("%d train=%f" % (i, ml_metrics.auc(np.array(train[train_i].Insult),ypred)))
 	ypred = clf.predict(train[test_i].Comment)
+        save_predictions(ypred,i)
 	est = ml_metrics.auc(np.array(train[test_i].Insult),ypred)
 	logging.info("test %f" % est)
 	ss += est
@@ -73,7 +87,13 @@ for param_name in sorted(parameters.keys()):
 	logging.info("\tL %s: %r" % (param_name, best_parameters[param_name]))
 ypred = clf.predict(leaderboard.Comment)
 submission = pandas.read_table('Data/sample_submission_null.csv',sep=',')
+ypred[ypred > 1] = 1
+ypred[ypred < 0] = 0
 submission['Insult'] = ypred
+
+
+
+
 for x in itertools.count(1):
 		filename = "submissions/submission%d.csv" % x
 		if os.path.exists(filename):
