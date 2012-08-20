7f13dd5cea2b6bf55f3af99bb2e07a863f086ae9
Chris Brew <cbrew@acm.org>
2012-08-20 07:40:12 -0400

    self training extras

--- vectorize.py
+++ vectorize.py
@@ -1,7 +1,57 @@
 """
 Code for the insults competition run by Kaggle in August 2012.
 
-Winning combination is grid search + sgd regressor.
+So far, winning combination is grid search + sgd regressor.
+
+
+Best run appears to be this one, with 2750 iterations. These parameters are chosen for
+all folds. I like the stability
+
+2012-08-18 12:57:42,500 : INFO : fold 0
+2012-08-18 13:11:59,331 : INFO : 	0 clf__alpha: 1.2589254117941662e-07  = 10**6.9
+2012-08-18 13:11:59,332 : INFO : 	0 clf__n_iter: 2750
+2012-08-18 13:11:59,332 : INFO : 	0 clf__penalty: 'l1'
+2012-08-18 13:11:59,332 : INFO : 	0 vect__analyzer: 'char'
+2012-08-18 13:11:59,332 : INFO : 	0 vect__lowercase: False
+2012-08-18 13:11:59,332 : INFO : 	0 vect__max_n: 5
+
+Uses the default squared loss, eta and learning rate. Should really try huber...
+
+
+
+Performance by fold:
+
+2012-08-18 13:12:14,983 : INFO : 0 test 0.921842
+2012-08-18 13:26:11,407 : INFO : 1 test 0.889459
+2012-08-18 13:39:16,335 : INFO : 2 test 0.888655
+2012-08-18 13:52:58,508 : INFO : 3 test 0.900914
+2012-08-18 14:06:45,541 : INFO : 4 test 0.887678
+
+Expected.
+
+2012-08-18 14:06:45,541 : INFO : Expected score 0.897710
+
+Ideas
+-----
+
+- use character n-grams because they are robust and simple.
+- do self-training using current best estimate
+- use SGD regression, and tune the bejazus out of it.
+- use custom classifiers based on sklearn raw ones.
+
+Questions
+=========
+- why not a classifier?
+- what is the best thing to do about the self-training?
+- did we really self-train the first time? Maybe we did, but it doesn't work
+- will it help to self-train and cross-validate?
+
+Desired interface
+=================
+
+Read set of training files.
+Read grid configuration.
+
 
 """
 import pandas
@@ -12,9 +62,9 @@ import os
 import itertools
 import logging
 import datetime
-import pickle
 import random
 import joblib
+
 
 self_train = True
 
@@ -27,6 +77,29 @@ best = pandas.read_table('best/best.csv',sep=',')
 
 
 leaderboard = pandas.read_table('Data/test.csv',sep=',')
+
+
+def round_predictions(ypred):
+	"""
+	normalize range of predictions to 0-1
+	"""
+	yrange = ypred.max() - ypred.min()
+	ypred /= yrange
+	ypred -= ypred.min()
+	# protection against rounding, ENSURE nothing out of range.
+	ypred[ypred > 1] = 1
+	ypred[ypred < 0] = 0
+	return ypred
+	
+	
+class MySGDRegressor(linear_model.SGDRegressor):
+	"""
+	Ensure that predictions are suitably rounded. Doing it here makes sure
+	that the quantity that is optimized is also the one that is wanted.
+	"""
+	def predict(self,X):
+		return round_predictions(linear_model.SGDRegressor.predict(self,X))
+
 
 class MyLogisticRegression(linear_model.LogisticRegression):
     def predict(self, X):
@@ -58,34 +131,25 @@ grid["sgd_w"] = {
 
 
 
+
+# Grid for SGD using character n-grams
+
 grid["sgd_c"] = {
-    # uncommenting more parameters will give better exploring power but will
-    # increase processing time in a combinatorial way
-    # 'vect__max_df': (0.5, 0.75, 1.0),
     'vect__analyzer': ['char'],
     'vect__lowercase': [False],
-    # 'vect__max_features': (None, 10, 50, 100, 500,1000,5000, 10000, 50000),
     'vect__max_n': (5,),  # words,bigrams,trigrams, etc.
-    #    'tfidf__use_idf': (True, False),
-    #    'tfidf__norm': ('l1', 'l2'),
-    # 'clf__C': (1e-2,1,1e+2),
-    # 'clf__C': (10,15,20,21,22,23,24,25,26,27,28,29,30,35,40,45,50,55,60,65,70,75,80,85,100,1000),
-    # 'clf__tol': (1e-3,1e-4),
+		'clf__loss': ("huber",),
     'clf__penalty': ("l1", ),
-    'clf__alpha': 10.0**-np.linspace(6.0,8.0,num=5),
-		# 'clf__alpha': 10.0**-np.arange(6.5,8.5,step=1.0),
-    'clf__n_iter': (2000,3000),
+    'clf__alpha': 10.0**-np.linspace(6.8,7.2,num=5),
+    'clf__n_iter': (3500, ),
 }
 
 
 fixed = {}
 fixed['sgd_c'] = {"shuffle": True}
 fixed['sgd_w'] = {}
-
-
-
-clfs = dict(sgd=(linear_model.SGDRegressor,fixed['sgd_w'],grid["sgd_w"]),
-            sgd_char=(linear_model.SGDRegressor,fixed['sgd_c'],grid["sgd_c"]))
+clfs = dict(sgd=(MySGDRegressor,fixed['sgd_w'],grid["sgd_w"]),
+            sgd_char=(MySGDRegressor,fixed['sgd_c'],grid["sgd_c"]))
 
 
 clfc,clfix,clfp = clfs["sgd_char"]
@@ -119,8 +183,6 @@ kf = cross_validation.KFold(len(train),5,indices=False)
 for i,(train_i,test_i) in enumerate(kf):
 	ftrain = train[train_i]
 	if self_train:
-
-
 			# when we self_train, crucial to shuffle before cross-validation
 			logging.info('original training %r',ftrain.shape)
 			ftrain = pandas.concat([ftrain,best],ignore_index=True)
@@ -146,18 +208,46 @@ for i,(train_i,test_i) in enumerate(kf):
 	
 logging.info('Expected score %f' % (ss/n))
 logging.info("Starting leaderboard")
-clf.fit(train.Comment,train.Insult)
-best_parameters = clf.best_estimator_.get_params()
-for param_name in sorted(clfp.keys()):
-	logging.info("\tL %s: %r" % (param_name, best_parameters[param_name]))
-ypred = clf.predict(leaderboard.Comment)
-submission = pandas.read_table('Data/sample_submission_null.csv',sep=',')
-ypred[ypred > 1] = 1
-ypred[ypred < 0] = 0
-submission['Insult'] = ypred
+
+if self_train:
+		kf = cross_validation.KFold(len(best),5,indices=False)
+		predictions = []
+		for i,(train_i,test_i) in enumerate(kf):
+			fbtrain = best[train_i]
+			fbtest  = best[test_i]
+		
+			fbtrain = pandas.concat([train,fbtrain],ignore_index=True)
+			fbtrain['Temp'] = random.shuffle(range(fbtrain.shape[0]))
+			fbtrain = fbtrain.sort('Temp')
+			fbtrain['Temp']
+			logging.info('self  training %d %r' %(i,fbtrain.shape))
+				
+			clf.fit(fbtrain.Comment,fbtrain.Insult)
+			best_parameters = clf.best_estimator_.get_params()
+			for param_name in sorted(clfp.keys()):
+				logging.info("\tL %s: %r" % (param_name, best_parameters[param_name]))
+			predictions.append(pandas.Series(clf.predict(fbtest.Insult),index=fbtest.index))
+		# if I have done this right, should be in correct order of predictions
+		ypred = pandas.concat(predictions)
+		
+				
+		
+		
+else:
+	clf.fit(train.Comment,train.Insult)
+	best_parameters = clf.best_estimator_.get_params()
+	for param_name in sorted(clfp.keys()):
+		logging.info("\tL %s: %r" % (param_name, best_parameters[param_name]))
+	ypred = clf.predict(leaderboard.Comment)
+	
+
+
+
 
 
 # we create a submission...
+submission = pandas.read_table('Data/sample_submission_null.csv',sep=',')
+submission['Insult'] = ypred
 for x in itertools.count(1):
 		filename = "submissions/submission%d.csv" % x
 		if os.path.exists(filename):
@@ -166,6 +256,9 @@ for x in itertools.count(1):
 			submission.to_csv(filename,index=False)
 			logging.info("result saved to %s" % filename)
 			model_filename = "models/model%d" % x
+			# XXX following line does not save the right model when self-training.
+			# Instead, it saves the model for fold k-1 of the cross-val.
+			# work is needed to create a model based on all folds.
 			joblib.dump(clf.best_estimator_,model_filename,compress=3)
 			logging.info("model saved to %s" % model_filename)
 			
