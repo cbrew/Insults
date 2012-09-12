f75906e56bc7e20f99978c8ae7e70dd5b65d8dfb
Chris Brew <cbrew@acm.org>
2012-09-12 07:57:51 -0400

    packaged in single class

--- torus.py
+++ torus.py
@@ -13,23 +13,32 @@ import matplotlib.animation as animation
 import matplotlib.cm as cm
 
 
-
-
-
 class Board:
-	def __init__(self):
+	def __init__(self,boardx=50,boardy=50):
 		self.filter = np.array([[1,1,1],[1,10,1],[1,1,1]],dtype='float')
-		self.board = np.zeros((40,80),dtype=bool)	
-		self.add_glider()
-		self.add_glider(x=5,y=20)
+		self.board = np.zeros((boardx,boardy),dtype=bool)	
+		self.add_glider(x=0,y=boardy/2,orientation='se')
+		self.add_glider(x=boardx/2,y=0,orientation='sw')
+		self.add_glider(x=3*boardx/4,y=boardy-1,orientation='nw')
+		self.add_glider(x=0,y=boardy-1,orientation='ne')
 		self.fig1 = plt.figure()
 	
 	def animation(self):
 		return animation.FuncAnimation(self.fig1, self.update , frames=500,init_func=self.image, interval=50, blit=True, repeat_delay=1000)
 
-	def add_glider(self, x=0, y=0): 
-		for i, j in  [(0,1), (1,2), (2,0), (2,1), (2,2)]: self.board[x+i, y+j] = 1.0
+	def add_glider(self, x=0, y=0,orientation='se'): 
+		assert orientation in ('se','sw','ne','nw')
+		for i, j in  [(0,1), (1,2), (2,0), (2,1), (2,2)]: 
+			if orientation == 'se':
+				pass
+			elif orientation =='sw':
+				i = -i
+			elif orientation == 'ne':
+				j = -j
+			elif orientation == 'nw':
+				i,j = -i,-j
 
+			self.board[x+i, y+j] = 1.0
 	def image(self):
 		self.im = plt.imshow(1.0-board.board,cmap=cm.Greys_r)
 		return (self.im, )
@@ -39,9 +48,10 @@ class Board:
 		self.im.set_array(1.0-self.board)
 		return (self.im, )
 
-board = Board()
-ani = board.animation()
-plt.show()
+if __name__ == "__main__":
+	board = Board()
+	ani = board.animation()
+	plt.show()
 
 
 
